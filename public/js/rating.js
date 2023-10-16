da4984483b1c9b42402a57889437d12f7631d297
Pa <137225158+pvang1987@users.noreply.github.com>
2023-10-16 00:32:52 -0500

    fixing image issue and updating rating code

--- public/js/rating.js
+++ public/js/rating.js
@@ -1,41 +1,45 @@
-const rateRecipeButton = document.getElementById('rateRecipeButton');
-const ratingForm = document.getElementById('ratingForm');
-const submitRatingButton = document.getElementById('submitRating');
-const ratingInput = document.getElementById('rating');
+document.addEventListener('DOMContentLoaded', function () {
+  const rateRecipeButton = document.getElementById('rateRecipeButton');
+  const ratingForm = document.getElementById('ratingForm');
+  const submitRatingButton = document.getElementById('submitRating');
+  const ratingInput = document.getElementById('rating');
 
-rateRecipeButton.addEventListener('click', function() {
-  ratingForm.style.display = 'block';
-});
+  rateRecipeButton.addEventListener('click', function () {
+    rateRecipeButton.style.display = 'none'; // Hide the "Rate this recipe" button
+    ratingForm.style.display = 'block'; // Show the rating form
+  });
 
-submitRatingButton.addEventListener('click', async function() {
-  const newRating = ratingInput.value;
+  submitRatingButton.addEventListener('click', async function () {
+    const newRating = ratingInput.value;
 
-  const currentPath = window.location.pathname;
-  const match = currentPath.match(/^\/project\/(\d+)$/);
+    // Your existing rating submission logic here
+    const currentPath = window.location.pathname;
+    const match = currentPath.match(/^\/project\/(\d+)$/);
 
-  if (match) {
-    const itemId = parseInt(match[1], 10);
-    // Make a PUT request to update the rating
-    try {
-      const response = await fetch(`/api/recipes/${itemId}`, {
-        method: 'PUT',
-        body: JSON.stringify({ rating: parseFloat(newRating) }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    if (match) {
+      const itemId = parseInt(match[1], 10);
+      // Make a PUT request to update the rating
+      try {
+        const response = await fetch(`/api/recipes/${itemId}`, {
+          method: 'PUT',
+          body: JSON.stringify({ rating: parseFloat(newRating) }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
 
-      if (response.ok) {
-        console.log('Rating updated successfully');
-      } else {
-        console.error('Failed to update rating');
+        if (response.ok) {
+          console.log('Rating updated successfully');
+        } else {
+          console.error('Failed to update rating');
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
       }
-    } catch (error) {
-      console.error('An error occurred:', error);
+    } else {
+      console.error('Unable to extract recipe ID from the URL');
     }
-  } else {
-    console.error('Unable to extract recipe ID from the URL');
-  }
 
-  ratingForm.style.display = 'none';
+    ratingForm.style.display = 'none'; // Hide the rating form after submission
+  });
 });
